9e766778e4f1c14a542598987b3f086316461995
Luca <lucasimonetti0612@gmail.com>
2025-09-25 18:06:23 -0300

    feat: add register screen with UI

---
+++ presentation/Register.py
@@ -0,0 +1,23 @@
+from PyQt6.QtWidgets import QApplication,QWidget,QPushButton,QLineEdit,QLabel,QMainWindow,QTableWidgetItem,QDialog,QFileDialog,QMessageBox
+from screens.Register_ui import Ui_Dialog
+from business.Logic import registrarUsuario
+
+class RegisterWindow(QDialog, Ui_Dialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.RegistroBtn.clicked.connect(self.RegistroBtnClick)
+
+    def RegistroBtnClick(self):
+        username = self.UserTxt.text().strip()
+        password = self.PassTxt.text().strip()
+
+        if not username or not password:
+            QMessageBox.warning(self, "Error", "No puede haber campos vacios")
+            return
+        if registrarUsuario(username, password):
+            QMessageBox.information(self, "Exito", "Registro exitoso")
+            self.accept()
+        else:
+            QMessageBox.warning(self, "Error", "El usuario ya existe")
